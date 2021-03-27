ecc15e6bb4931875f788235dec66f730d571a7f2
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-26 20:29:28 -0400

    fixed errors in server leading to lyrics not appearing

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const SpotifyWebApi = require("spotify-web-api-node");
-const lyricsFinder = require("lyricsFinder");
+const lyricsFinder = require("lyrics-finder");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlendcoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
